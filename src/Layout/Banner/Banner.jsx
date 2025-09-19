8724571c1149f4858832e8d1d4b076194f852cd7
Trinh GIang <trinhgiang3112@gmail.com>
2025-09-20 04:26:55 +0700

    update

--- src/Layout/Banner/Banner.jsx
+++ src/Layout/Banner/Banner.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import workImage from "../../assets/work.png";
+
 import "./Banner.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
@@ -35,7 +35,7 @@ export default function Banner() {
           </button>
         </div>
         <div data-aos="fade-left" className="img">
-          <img src={workImage} alt="student" />
+          <img src="/public/image/work.png" alt="student" />
         </div>
       </div>
     </div>
